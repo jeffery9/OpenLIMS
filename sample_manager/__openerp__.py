297c749be4a0971a5e3d277ccbd2da1be32958b4
jeffery <jeffery9@gmail.com>
2013-08-22 20:51:19 +0800

    init sample_manager module

---
+++ sample_manager/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sample Manager',
+    'version': '1.0',
+    'category': '',
+    'description': """""",
+    'author': ' ()',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'active': False,
+    'installable': True,
+}
+
