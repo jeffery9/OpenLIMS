7c1ddfc2877c625ea50edfb39f4587c7f72d3aae
jeffery <jeffery9@gmail.com>
2013-08-23 16:52:56 +0800

    template use to add property key to sample

--- sample_manager/__openerp__.py
+++ sample_manager/__openerp__.py
@@ -4,13 +4,22 @@
     'name': 'Sample Manager',
     'version': '1.0',
     'category': '',
-    'description': """""",
-    'author': ' ()',
+    'description': """
+
+    some descripitons
+
+    """,
+    'author': 'Jeffery Chen Fan',
     'website': '',
     'license': 'AGPL-3',
-    'depends': [],
+    'depends': [
+        'product',
+    ],
     'init_xml': [],
     'update_xml': [],
+    'data': [
+        'sample_view.xml'
+    ],
     'demo_xml': [],
     'active': False,
     'installable': True,
--- sample_manager/sample.py
+++ sample_manager/sample.py
@@ -8,35 +8,106 @@ from openerp.tools.translate import _
 from openerp import netsvc
 
 
-class sample(osv.osv):
-    _name = 'sample.sampler'
-    _description = 'Sample'
-    # _inherit = ['mail.thread', 'ir.needaction_mixin']
+class sample_sample_property(osv.osv):
+    _name = 'sample.sample.property'
+    _description = 'sample property'
 
     _columns = {
-        'code': fields.char('code', size=64),
-        'name': fields.char('Reference', size=64),
-        'company_id': fields.many2one('res.company', 'Company', required=True, readonly=True,
-                                      states={'draft': [('readonly', False)]}),
+        'key': fields.char('key', size=64),
+        'value': fields.char('Value', size=64),
+        'sample_id': fields.many2one('sample.sample', 'Sample',
+                                       required=True),
+    }
+
+    # _defaults = {}
+
+
+sample_sample_property()
+
+
+class sample_sample_property_key(osv.osv):
+    _name = 'sample.sample.property.key'
+    _description = 'sample property key'
+
+    _columns = {
+        'key': fields.char('key', size=64),
+        'template_id': fields.many2one('sample.sample.template', 'Template',
+                                       required=True),
+    }
+
+    # _defaults = {}
+
+
+sample_sample_property()
+
+
+class sample_sample_template(osv.osv):
+    _name = 'sample.sample.template'
+    _description = 'sample template'
+
+    _columns = {
+        'name': fields.char('Template Name', size=64),
+        'properties': fields.one2many('sample.sample.property.key',
+                                      'template_id',
+                                      'Properties',
+                                      required=True),
+        'samples': fields.one2many('sample.sample', 'template_id',
+                                   'Samples',
+                                   required=True),
+        'company_id': fields.many2one('res.company', 'Company', required=True,
+                                      readonly=True),
+
     }
 
     _defaults = {
-        'state': lambda *a: 'draft',
-        'company_id': lambda self, cr, uid, c: self.pool.get('res.company')._company_default_get(cr, uid,
-                                                                                                 'sample.sample',
-                                                                                                 context=c),
+        'company_id': lambda self, cr, uid, ctx: self.pool.get(
+            'res.company')._company_default_get(cr, uid,
+                                                'sample.sample',
+                                                context=ctx),
     }
 
-    # _order = ''
-sample()
+
+sample_sample_template()
 
 
-class product(osv.osv):
-    _inherit = 'product.product'
+class sample_sample(osv.osv):
+    _name = 'sample.sample'
+    _description = 'Sample'
+
+    _columns = {
+        'code': fields.char('Code', size=64),
+        'template_id': fields.many2one('sample.sample.template', 'Template',
+                                       required=True),
+        'properties': fields.one2many('sample.sample.property',
+                                      'sample_id',
+                                      'Properties',
+                                      required=True),
+        'company_id': fields.many2one('res.company', 'Company', required=True,
+                                      readonly=True,
+        ),
+    }
+
+    _defaults = {
+        'company_id': lambda self, cr, uid, ctx: self.pool.get(
+            'res.company')._company_default_get(cr, uid,
+                                                'sample.sample',
+                                                context=ctx),
+    }
+
+
+sample_sample()
+
+
+class product_product(osv.osv):
+    _inherit = ['product.product']
+    _name = 'product.product'
     _columns = {
         'is_sample': fields.boolean('Is Sample'),
     }
 
     # _order = ''
-product()
+
+
+product_product()
+
 
